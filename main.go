4650008b99ae0dba97db983a9a6cd99ab8f6df92
Wind <jiangmail@126.com>
2015-05-20 17:55:45 +0800

    mdf main.go

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 		`))
 	})
 
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
 }
